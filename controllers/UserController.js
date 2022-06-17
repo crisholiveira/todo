190b6cd03a11291fb960de85113e60c74839970b
crisholiveira <cris.ho.soares@gmail.com>
2022-06-17 20:22:15 -0300

    registro do usuário com retorno de token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,20 +2,52 @@ const User = require("../models/User")
 
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-const jwtSecret = process.env.JWT_SECRET 
+const jwtSecret = process.env.JWT_SECRET
 //função para gerar token
 const generateToken = (id) => {
-  return jwt.sign({id}, jwtSecret, {
+  return jwt.sign({ id }, jwtSecret, {
     expiresIn: "7d",
   })
 }
 
 //registrar e logar
 const register = async (req, res) => {
-  res.send("Registro")
+  const { name, email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user) {
+    res.status(422).json({ errors: ["Este e-mail já está cadastrado, por favor, utilize outro!"] })
+    return
+  }
+
+  //gerar a senha hash
+  const salt = await bcrypt.genSalt()
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  //criar o usuario
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+    profile: "user"
+  })
+
+  //criado com sucesso
+
+  if (!newUser) {
+    res.status(422).json({ errors: ["Houve um erro, tente mais tarde!"] })
+    return
+  }
+  res.status(201).json({
+    id: newUser.id,
+    token: generateToken(newUser.id)
+  })
+
+
 }
 
 //exportar as funções para depois poder importar nas rotas
-module.exports= {
+module.exports = {
   register,
 }
